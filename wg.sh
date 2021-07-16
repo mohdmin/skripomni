4fd817ac0aea8d34db9af6c3077a19aab63e1989
OmniNET <55236299+OmniN3T@users.noreply.github.com>
2021-07-16 11:39:13 +0700

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -89,10 +89,10 @@ WG_RUNNING=$?
 
 # Tambahan
 cd /usr/bin
-wget -O add-wg "https://raw.githubusercontent.com/aryprabow/Script/main/add-wg.sh"
-wget -O del-wg "https://raw.githubusercontent.com/aryprabow/Script/main/del-wg.sh"
-wget -O cek-wg "https://raw.githubusercontent.com/aryprabow/Script/main/cek-wg.sh"
-wget -O renew-wg "https://raw.githubusercontent.com/aryprabow/Script/main/renew-wg.sh"
+wget -O add-wg "https://raw.githubusercontent.com/OmniN3T/AutoScriptVPS/main/add-wg.sh"
+wget -O del-wg "https://raw.githubusercontent.com/OmniN3T/AutoScriptVPS/main/del-wg.sh"
+wget -O cek-wg "https://raw.githubusercontent.com/OmniN3T/AutoScriptVPS/main/cek-wg.sh"
+wget -O renew-wg "https://raw.githubusercontent.com/OmniN3T/AutoScriptVPS/main/renew-wg.sh"
 chmod +x add-wg
 chmod +x del-wg
 chmod +x cek-wg
