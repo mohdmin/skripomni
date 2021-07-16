a3a9bb38a9fbea09b66b0d7ec4818d8495b2b940
OmniNET <55236299+OmniN3T@users.noreply.github.com>
2021-07-16 15:41:49 +0700

    Update sstp.sh

--- sstp.sh
+++ sstp.sh
@@ -25,9 +25,9 @@ touch /var/lib/premium-script/data-user-sstp
 country=ID
 state=DI Yogyakarta
 locality=Kulon Progo
-organization=OmniNET.com
-organizationalunit=OmniNET.com
-commonname=OmniNET.com
+organization=OmniNET.id
+organizationalunit=OmniNET.id
+commonname=id.OmniNET.id
 email=Hublawikipedia@gmail.com
 
 #install sstp
